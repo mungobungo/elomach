c0b9761443bc6ea8089e3dea9ad30b1d0ebcee3c
Oleg Taykalo <oleg.taykalo@gmail.com>
2015-05-11 00:53:34 +0300

    async loading now made using Rx framework. all heil Erik Meijer

--- js/models.js
+++ js/models.js
@@ -3,14 +3,34 @@ require('script!./three.js');
 require('script!./OBJLoader.js');
 require('script!./OBJMTLLoader.js');
 require('script!./MTLLoader.js');
-//require('script!./threex.spaceships.js');
+//var bacon = require('baconjs');
+var rx = require('rx');
 
 function models(scene) {
 
 	// instantiate a loader 
 	var loader = new THREE.OBJMTLLoader(); 
 
-	loadObjMtl(scene, loader, 'models/SpaceFighter02/SpaceFighter02');
+	var res = loadObjMtl(scene, loader, 'models/SpaceFighter02/SpaceFighter02');
+	
+	var subscription = res.subscribe(
+  		function (x) { 
+  			console.log('onNext: %s', x); 
+  	// 		x.traverse(function(x){
+			// if( object3d.material ){
+			// 	object3d.material.emissive.set('white')
+			// }
+		// })
+  			scene.add(x);
+  		},
+  		function (e) { console.log('onError: %s', e); },
+  		function () { console.log('onCompleted'); }
+  	);
+	// res.callback.onValue(function(val){ 
+	//  	console.log(val);
+	//  	
+	//  });
+		
 	
 }
 module.exports = models;
@@ -19,25 +39,34 @@ module.exports = models;
 
 
 function loadObjMtl(scene, loader, resource){
+	
+
+	//var exists = rx.Observable.fromCallback(callback);
+	// Function called when downloads progress 
+	var progress = function ( xhr ) { console.log( (xhr.loaded / xhr.total * 100) + '% loaded' ); };
+
+	// Function called when downloads error 
+	var error = function ( xhr ) { console.log( 'An error happened' ); }
+	
+	
+	var subject = new Rx.AsyncSubject();
 	// load an obj / mtl resource pair 
 	loader.load( 
 	// OBJ resource URL 
-	resource + '.obj', 
-	// MTL resource UR
-	resource + '.mtl',
-	// Function when both resources are loaded 
-	function ( object ) { 
-		object.traverse(function(object3d){
-			if( object.material ){
-				object.material.emissive.set('white')
-			}
-		})
-		scene.add( object ); 
-	}, 
-	// Function called when downloads progress 
-	function ( xhr ) { console.log( (xhr.loaded / xhr.total * 100) + '% loaded' ); }, 
-	// Function called when downloads error 
-	function ( xhr ) { console.log( 'An error happened' ); } );
+		resource + '.obj', 
+		// MTL resource UR
+		resource + '.mtl',
+		function(result) {
+            subject.onNext(result);
+            subject.onCompleted();
+    	}, 
+		progress, 
+		error 	
+	 );
+
+	
+
+	return subject.asObservable();
 
 }
 
